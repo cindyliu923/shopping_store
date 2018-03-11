350990bfe5f22d9b015818b2e0f00e1d6b13337c
Cindy Liu <cindyliu923@gmail.com>
2018-03-11 16:19:20 +0800

    add login process for admin / set up admin authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Default admin
+
+User.create(email: "root@admin.com", password: "rootadmin", role: "admin")
+puts "Default admin created!"
