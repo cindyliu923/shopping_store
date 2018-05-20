cc1cd7beb7c0e071dda63e79e112ec86e7adfb41
Cindy Liu <cindyliu923@gmail.com>
2018-05-20 22:42:15 +0800

    create order_item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520140222) do
+ActiveRecord::Schema.define(version: 20180520143152) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "quantity", default: 1
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20180520140222) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id", "product_id"], name: "index_order_items_on_order_id_and_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
