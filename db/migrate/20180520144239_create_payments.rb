a319f4491a98297458fc09d9bdc0d7b6e53854f5
Cindy Liu <cindyliu923@gmail.com>
2018-05-20 22:47:36 +0800

    create payment model

---
+++ db/migrate/20180520144239_create_payments.rb
@@ -0,0 +1,15 @@
+class CreatePayments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :payments do |t|
+      t.integer :order_id
+      t.integer :sn, null: false
+      t.string :payment_method
+      t.integer :amount
+      t.datetime :paid_at
+      t.text :params
+      t.timestamps
+    end
+
+    add_index :payments, :order_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520143152) do
+ActiveRecord::Schema.define(version: 20180520144239) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "quantity", default: 1
@@ -48,6 +48,18 @@ ActiveRecord::Schema.define(version: 20180520143152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "sn", null: false
+    t.string "payment_method"
+    t.integer "amount"
+    t.datetime "paid_at"
+    t.text "params"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_payments_on_order_id"
   end
 
   create_table "products", force: :cascade do |t|
