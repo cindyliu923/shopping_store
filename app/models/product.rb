90c4ab43016b31249533bf57b57f9f69d0fc0be8
Cindy Liu <cindyliu923@gmail.com>
2018-05-21 21:23:01 +0800

    setup relationship between product, cart and cart_item

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   mount_uploader :image, PhotoUploader
   validates_presence_of :name, :price
 
+  has_many :crat_items, dependent: :destroy
+  has_many :cart, through: :crat_items
 end
