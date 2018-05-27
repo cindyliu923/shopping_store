e296a72f50ee3c5574a0892202b3f11759fcf6c9
Cindy Liu <cindyliu923@gmail.com>
2018-05-27 22:19:53 +0800

    modify delete product from cart to AJAX

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,15 +17,13 @@ class ProductsController < ApplicationController
       :name => @product.name, 
       :price => @product.price 
     }
-  #  redirect_back(fallback_location: root_path)
   end
 
   def remove_from_cart
     product = Product.find(params[:id])
     cart_item = current_cart.cart_items.find_by(product_id: product)
     cart_item.destroy
-
-    redirect_back(fallback_location: root_path)
+    render :json => { :id => product.id }
   end
 
 end
