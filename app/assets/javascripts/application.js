c6ac5e3fd3bfab0344aa25aff93620da2c73c203
Cindy Liu <cindyliu923@gmail.com>
2018-06-02 17:40:12 +0800

    update bootstrap to 4 version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,8 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery
+//= require jquery3
+//= require popper
 //= require jquery_ujs
 //= require bootstrap-sprockets
 
