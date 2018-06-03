8a223af6cd37d12137bdb032a183f13c95d934fc
Cindy Liu <cindyliu923@gmail.com>
2018-06-03 10:37:19 +0800

    setup relation between order, order_item, payment and user

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  validates_presence_of :name, :address, :phone, :payment_status, :shipping_status
+
+  has_many :order_items, dependent: :destroy
+  has_many :products, through: :order_items
+  has_many :payments, dependent: :destroy
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :orders, dependent: :restrict_with_error
+
   def admin?
     self.role == "admin"
   end
