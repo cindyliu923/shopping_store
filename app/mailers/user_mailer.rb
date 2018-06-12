43dac0436f53591fdb2e3df471d38a5cfa369829
Cindy Liu <cindyliu923@gmail.com>
2018-06-12 22:57:12 +0800

    setup mail for order paid and shipped

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,4 +9,20 @@ class UserMailer < ApplicationMailer
     subject: "Cindy Shopping store | 訂單成立: #{@order.id}"
   end
 
+  def notify_order_shipped(order)
+    @order = order
+    @content = "We are pleased to inform you that your order has been shipped!"
+
+    mail to: order.user.email,
+    subject: "Cindy Shopping store | 訂單寄出: #{@order.id}"
+  end
+
+  def notify_order_paid(order)
+    @order = order
+    @content = "Your order has been paid."
+
+    mail to: order.user.email,
+    subject: "Cindy Shopping store | 訂單已付款: #{@order.id}"
+  end
+
 end
